9c1b29c8da206ce6cdced063ef2c934361ff25da
Dev Uppin <devuppin@gmail.com>
2022-11-09 12:41:38 -0800

    added for loop

--- jscripts/script.js
+++ jscripts/script.js
@@ -1,4 +1,9 @@
-for (let i = 0; i < 1000000; i++) {
-    console.log(i);
-    
+// for (let i = 0; i < 1000000; i++) {
+//     console.log(i);
+
+// }
+
+for (let i = 1; i <= 10; i++) {
+    console.log(Math.pow(2, i))
 }
+
